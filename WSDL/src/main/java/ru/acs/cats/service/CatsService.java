4f119d58843ec2b0b83e89f58bc96fb3e79e56ed
alexey-98 <UnDuD8dfZH>
2014-10-09 13:38:34 +0600

    1

--- WSDL/src/main/java/ru/acs/cats/service/CatsService.java
+++
@@ -1,9 +0,0 @@
-package ru.acs.cats.service;
-
-import ru.acs.entities.CatEntity;
-
-public interface CatsService {
-	public CatEntity create( CatEntity cat );
-	public CatEntity findById( int id );
-	public Integer update( CatEntity cat ) throws Exception;
-}
